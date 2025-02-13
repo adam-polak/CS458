de1a85ee06d276b7c62995800a67a40ce35f99ca
Adam <adamjpolak@gmail.com>
2025-02-13 14:45:19 -0600

    trim space from string

--- Project/src/Main.java
+++ Project/src/Main.java
@@ -3,7 +3,20 @@ import lib.MIPSInstructionFactory;
 
 public class Main {
     public static void main(String[] args) {
-        String mipsAsm = args[0];
+        String mipsAsm = formatString(args[0]);
         MIPSInstruction instruction = MIPSInstructionFactory.create(mipsAsm);
+    }
+
+    private static String formatString(String str) {
+        str = str.trim();
+        StringBuilder ans = new StringBuilder();
+        char[] arr = str.toCharArray();
+        for (char c : arr) {
+            if (c != ' ') {
+                ans.append(c);
+            }
+        }
+
+        return ans.toString();
     }
 }
