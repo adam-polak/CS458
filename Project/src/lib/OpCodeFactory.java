14d26a91f45eb6b3fcde5b5d3eccf6cbc304b491
Adam <adamjpolak@gmail.com>
2025-02-13 10:59:42 -0600

    fill map with required instructions

--- Project/src/lib/OpCodeFactory.java
+++ Project/src/lib/OpCodeFactory.java
@@ -6,6 +6,20 @@ public class OpCodeFactory {
 
     private static final HashMap<String, Integer> map = new HashMap<>(){{
         put("add", 1); // TODO add all op codes
+        put("addiu", 2);
+        put("and", 3);
+        put("andi", 4);
+        put("beq", 5);
+        put("bne", 6);
+        put("j", 7);
+        put("lui", 8);
+        put("lw", 9);
+        put("or", 10);
+        put("ori", 11);
+        put("slt", 12);
+        put("sub", 13);
+        put("sw", 14);
+        put("syscall", 15);
     }};
 
     public static int getValue(String str) {
