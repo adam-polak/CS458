7fde58d469754306b105a6d709a9518fcd3b7f3d
Adam <adamjpolak@gmail.com>
2025-02-13 10:13:59 -0600

    updating mips factory

--- Project/src/Main.java
+++ Project/src/Main.java
@@ -1,5 +1,10 @@
+import lib.MIPSInstruction;
+import lib.MIPSInstructionFactory;
+import lib.MIPSStringType;
+
 public class Main {
     public static void main(String[] args) {
         String mipsAsm = args[0];
+        MIPSInstruction instruction = MIPSInstructionFactory.create(mipsAsm);
     }
 }
--- Project/src/lib/MIPSInstructionFactory.java
+++ Project/src/lib/MIPSInstructionFactory.java
@@ -1,6 +1,27 @@
 package lib;
 
 public class MIPSInstructionFactory {
+
+    public static MIPSInstruction create(String str) {
+        if(str.startsWith("0x")) {
+            return create(str, MIPSStringType.Hex);
+        } else if(isBinary(str)) {
+            return create(str, MIPSStringType.Binary);
+        } else {
+            return create(str, MIPSStringType.String);
+        }
+    }
+
+    private static boolean isBinary(String str) {
+        for(int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if(c == '0' || c == '1') continue;
+            return false;
+        }
+
+        return true;
+    }
+
     public static MIPSInstruction create(String str, MIPSStringType type) {
         if(type == MIPSStringType.String) {
             return fromString(str);
